bec248845dc8db72b126c300bcc432049c157b61
Felipe Vidal <felipe.vidal.mesquita@gmail.com>
2023-03-21 16:38:47 -0300

    UI

--- pages/protocolos/new.tsx
+++ pages/protocolos/new.tsx
@@ -50,7 +50,7 @@ const UserCreate = () => {
         form.num_processo.trim().length &&
         form.nome.trim().length &&
         form.assunto.trim().length
-        ) {
+      ) {
         const submitter = document.activeElement as HTMLButtonElement;
         setNotification(notificationInitialState);
         setIsLoading(true);
@@ -269,17 +269,39 @@ const UserCreate = () => {
               title="Salvar o protocolo sem imprimir."
               value="SAVE"
               disabled={isLoading}
-              className="flex-1 rounded-[10px] bg-green-600 p-1 text-lg font-light text-white hover:bg-green-500 disabled:bg-green-400"
+              className="flex flex-1 items-center justify-between gap-2 rounded-[10px] bg-green-600 p-1 px-3 text-lg font-light text-white hover:bg-green-500 disabled:bg-green-400"
             >
-              {isLoading ? "Salvando..." : "Salvar"}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.026A2 2 0 0 0 2 14h6.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586l-1.239-.757ZM16 4.697v4.974A4.491 4.491 0 0 0 12.5 8a4.49 4.49 0 0 0-1.965.45l-.338-.207L16 4.697Z" />
+                <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm-1.993-1.679a.5.5 0 0 0-.686.172l-1.17 1.95-.547-.547a.5.5 0 0 0-.708.708l.774.773a.75.75 0 0 0 1.174-.144l1.335-2.226a.5.5 0 0 0-.172-.686Z" />
+              </svg>
+              <span>{isLoading ? "Salvando..." : "Salvar"}</span>
+              <span></span>
             </button>
             <button
               title="Salvar e imprimir o protocolo."
               value="PRINT"
               disabled={isLoading}
-              className="flex-1 rounded-[10px] bg-blue-600 p-1 text-lg font-light text-white hover:bg-blue-500 disabled:bg-blue-400"
+              className="flex flex-1 items-center justify-between gap-2 rounded-[10px] bg-blue-600 p-1 px-3 text-lg font-light text-white hover:bg-blue-500 disabled:bg-blue-400"
             >
-              {isLoading ? "Salvando..." : "Salvar & Imprimir"}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5zm6 8H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z" />
+                <path d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
+              </svg>
+              <span>{isLoading ? "Salvando..." : "Salvar & Imprimir"}</span>
+              <span></span>
             </button>
           </div>
         </form>
