8728d181065422e8bdeb668aed981689930d9b36
Felipe Vidal <vidalfelipe2018@gmail.com>
2024-07-18 11:09:16 -0300

    Capa Design update

--- lib/capas.ts
+++ lib/capas.ts
@@ -6,26 +6,26 @@ export function printCapa(capa: Capa) {
   win.document.write(`
             <html>
               <head><title>Capa</title></head>
-              <body style="margin: 0; padding: 0; display: flex; flex-direction: column; justify-content: flex-start; font-family: Arial, Helvetica, sans-serif;">
-                <p style="margin: 0.5rem; margin-left: 3rem; text-align: start; font-size: 16px;"><b>PROTOCOLO N°:</b> <span>${
+              <body style="margin: 0; border: 2px solid black; padding: 3rem 5rem; display: flex; flex-direction: column; justify-content: flex-start; font-family: Arial, Helvetica, sans-serif;">
+                <p style="margin: 0.5rem; margin-left: 0; text-align: start; font-size: 14px;"><b>PROTOCOLO N°:</b> <span>${
                   capa.num_protocolo
                 }</span></p>
-                <p style="margin: 0.5rem; margin-left: 3rem; text-align: start; font-size: 16px;"><b>DISTRIBUIÇÃO:</b> <span>${format(
+                <p style="margin: 0.5rem; margin-left: 0; text-align: start; font-size: 14px;"><b>DISTRIBUIÇÃO:</b> <span>${format(
                   addHours(new Date(capa.distribuicao), 3),
                   "dd/MM/yyyy"
                 )}</span></p>
                 <p style="margin-top: 2rem; text-align: start; font-size: 24px; font-weight: bold; align-self:center;">PREFEITURA MUNICIPAL DE MESQUITA</p>
-                <img src="" alt="Logo" width="128" height="128" style="align-self: center; margin-top: 0.25rem; margin-bottom: 5rem;">
-                <p style="margin: 1rem; border: 1px solid black; padding: 1rem; padding-top: 0.5rem; padding-left: 0.5rem; text-align: start; font-size: 16px;"><b>VOLUME:</b> <span>${
+                <img src="" alt="Logo" width="181" height="181" style="align-self: center; margin-top: 0.25rem; margin-bottom: 5rem;">
+                <p style="margin: 1rem; padding: 0.25rem 0.5rem; width: fit-content; margin-inline: auto; border: 1px solid black; text-align: start; font-size: 14px;"><b>VOLUME:</b> <span>${
                   capa.volume
                 }</span></p>
-                  <p style="margin: 1rem; border: 1px solid black; padding: 1rem; padding-top: 0.5rem; padding-left: 0.5rem; text-align: start; font-size: 16px;"><b>REQUERENTE:</b> <span>${
+                  <p style="margin: 1rem; padding: 0.25rem; width: 100%; margin-inline: auto; border: 1px solid black; padding-left: 0.5rem; text-align: start; font-size: 14px;"><b>REQUERENTE:</b> <span>${
                     capa.requerente
                   }</span></p>
-                <p style="margin: 1rem; border: 1px solid black; padding: 1rem; padding-top: 0.5rem; padding-left: 0.5rem; text-align: start; font-size: 16px;"><b>ASSUNTO:</b> <span>${
+                <p style="margin: 1rem; padding: 0.25rem; width: 100%; margin-inline: auto; border: 1px solid black; padding-left: 0.5rem; text-align: start; font-size: 14px;"><b>ASSUNTO:</b> <span>${
                   capa.assunto
                 }</span></p>
-                <p style="margin: 1rem; border: 1px solid black; padding: 1rem; padding-top: 0.5rem; padding-left: 0.5rem; text-align: start; font-size: 16px;"><b>OBSERVAÇÃO:</b> <span>${
+                <p style="margin: 1rem; padding: 0.25rem; width: 100%; margin-inline: auto; border: 1px solid black; padding-left: 0.5rem; text-align: start; font-size: 14px;"><b>OBSERVAÇÃO:</b> <span>${
                   capa.observacao
                 }</span></p>
                 <script>
